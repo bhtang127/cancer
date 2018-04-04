ffe532014bf07b4f7373905bfc14f03b6eeb269c
IlyfTang <13300180071@fudan.edu.cn>
2018-04-04 18:29:57 -0400

    modulize

--- test.py
+++ test.py
@@ -15,52 +15,107 @@ def sampleUID(cycles, n_samples):
         else:
             maintain.append(sample)
     
-    return np.array(maintain, dtype=np.int32) 
+    return np.sort(np.array(maintain, dtype=np.int32))
 
-def get_mutation(samples_id, cycles, bases_per_amplicon, error_rate):
+def nsamples_generator(counts, total_samples):
+    events = np.sum(counts)
+    l = len(counts)
+    nsamples = [0 for i in range(l)]
+    for i in range(total_samples):
+        sample = np.random.randint(events)
+        loc = 0, s = 0
+        while s < sample:
+            s += counts[loc]
+            loc += 1
+        nsamples[loc] += 1
+    
+    return nsamples
+
+def translate_aux(id, cycles):
+    if cycles == 0:
+        return [0, 0]
+    if id % 2 == 1:
+        uid = translate_aux( int(id / 2), cycles - 1 )[1]
+        return [uid, uid]
+    else:
+        uid1 = translate_aux( int(id / 2), cycles - 1 )[0]
+        uid2 = id / 2 + 2 ** (cycles - 1)
+        return [uid1, uid2]
+
+def translate(samples_id, cycles):
     n = len(samples_id)
+    UID = np.array([ translate_aux(id, cycles) for id in samples_id], dtype = np.int32)
+    return UID
+
+def translate_well_aux(id, cycles):
+    if id == 0:
+        return [0, 1]
+    elif 0 < id and id < 2**(cycles-1):
+        return [1, 1]
+    else:
+        return translate_well_aux( id - 2**(cycles-1), cycles-1 )
+
+def translate_well(samples_id, cycles):
+    n = len(samples_id)
+    well = np.array([ translate_well_aux(id, cycles) for id in samples_id], dtype = np.bool)
+    return well
+
+def poisson_mutation(bases_per_amplicon, error_rate, ancestor_to_kid, mutation_count):
+    if isinstance( ancestor_to_kid, list ):
+        n = len(ancestor_to_kid)
+        poisson_mean = n * bases_per_amplicon * error_rate
+        total_count = np.random.poisson(poisson_mean)
+        if total_count == 0: return
+        mutated_order = np.random.randint(n, size = (total_count,))
+        for t in mutated_order:
+            if ancestor_to_kid[t] == 0:
+                mutation_count[t, 1] += 1
+            else:
+                mutation_count[t, 0] += 1
+        return
+
+    elif isinstance(ancestor_to_kid, dict):
+        n = len(ancestor_to_kid)
+        poisson_mean = n * bases_per_amplicon * error_rate
+        total_count = np.random.poisson(poisson_mean)
+        if total_count == 0: return
+        mutated_order = np.random.randint(n, size=(total_count,))
+        for loc in mutated_order:
+            for id in ancestor_to_kid[loc]:
+                mutation_count[id, 0] += 1 
+                mutation_count[id, 1] += 1
+    
+    else:
+        raise(TypeError)
+
+def ancestor_to_kid(samples_id, depth):
+    if depth == 0:
+        return [id % 2 for id in samples_id]
+    else:
+        denominator = 2 ** (depth-1)
+        ancestors = []
+        ancestors_to_kids = {}
+        order = 0
+        for id in samples_id:
+            kid = np.where(samples_id == id)[0][0]
+            prev_ancestor = int(id / denominator)
+            ancestor = int(prev_ancestor / 2)
+            if (ancestor + prev_ancestor) % 2 == 0:
+                continue
+            else:
+                if ancestor in ancestors:
+                    ancestors_to_kids[order].append(kid)
+                else:
+                    ancestors_to_kids[order] = [kid] 
+                    order += 1
+                    ancestors.append(ancestor)
+
+
+def get_mutation_per_molecule(samples_id, cycles, bases_per_amplicon, error_rate):
     mutation_count = np.zeros([n, 2], dtype = np.int16)
 
-    for i in range(cycles):
-        if i == 0:
-            poisson_mean = n * bases_per_amplicon * error_rate
-            total_count = np.random.poisson(poisson_mean)
-            if total_count == 0: continue
-            mutated_order = np.random.randint(n, size = (total_count,))
-            for t in mutated_order:
-                if samples_id[t] % 2 == 0:
-                    mutation_count[t, 1] += 1
-                else:
-                    mutation_count[t, 0] += 1
+    [ poisson_mutation(bases_per_amplicon, error_rate, ancestor_to_kid(samples_id, i), mutation_count) for i in range(cycles) ]
         
-        else:
-            denominator = 2 ** (i-1)
-            ancestors_to_kids = {}
-            
-            for id in samples_id:
-                prev_ancestor = int(id / denominator)
-                ancestor = int(prev_ancestor / 2)
-                if (ancestor + prev_ancestor) % 2 == 0:
-                    continue
-                else:
-                    if ancestor in ancestors_to_kids.keys():
-                        ancestors_to_kids[ancestor].append(id)
-                    else:
-                        ancestors_to_kids[ancestor] = [id]
-
-            keys = list(ancestors_to_kids)
-            n_ancestors = len(keys) 
-            poisson_mean = n_ancestors * bases_per_amplicon * error_rate
-            total_count = np.random.poisson(poisson_mean)
-            if total_count == 0: 
-                continue
-            mutated_order = np.random.randint(n_ancestors, size=(total_count,))
-            for o in mutated_order:
-                for mutated_id in ancestors_to_kids[ keys[o] ]:
-                    order = np.where(samples_id == mutated_id)[0][0]
-                    mutation_count[order, 0] += 1 
-                    mutation_count[order, 1] += 1
-
     return mutation_count
 
 if __name__ == '__main__':
