193caa4f92f80251a46bfbe167618406d1a8290e
Bohao Tang <Bohao Tang>
2018-04-04 21:21:00 -0400

    modulize

--- test.py
+++ test.py
@@ -5,6 +5,9 @@ import sys
 
 def sampleUID(cycles, n_samples):
     ## cycles < 32
+    if n_samples == 0:
+        return None
+
     init = np.random.randint(2**cycles)
     maintain = [init]
 
@@ -23,7 +26,8 @@ def nsamples_generator(counts, total_samples):
     nsamples = [0 for i in range(l)]
     for i in range(total_samples):
         sample = np.random.randint(events)
-        loc = 0, s = 0
+        loc = 0
+        s = 0
         while s < sample:
             s += counts[loc]
             loc += 1
@@ -43,22 +47,26 @@ def translate_aux(id, cycles):
         return [uid1, uid2]
 
 def translate(samples_id, cycles):
+    if samples_id is None:
+        return None
     n = len(samples_id)
     UID = np.array([ translate_aux(id, cycles) for id in samples_id], dtype = np.int32)
     return UID
 
-def translate_well_aux(id, cycles):
+def translate_well_aux(id, cycles, well):
     if id == 0:
-        return [0, 1]
+        return [0, well]
     elif 0 < id and id < 2**(cycles-1):
-        return [1, 1]
+        return [well, well]
     else:
-        return translate_well_aux( id - 2**(cycles-1), cycles-1 )
+        return translate_well_aux( id - 2**(cycles-1), cycles-1, well )
 
-def translate_well(samples_id, cycles):
+def translate_well(samples_id, cycles, well):
+    if samples_id is None:
+        return None
     n = len(samples_id)
-    well = np.array([ translate_well_aux(id, cycles) for id in samples_id], dtype = np.bool)
-    return well
+    translation = np.array([ translate_well_aux(id, cycles, well) for id in samples_id ], dtype = np.int16)
+    return translation
 
 def poisson_mutation(bases_per_amplicon, error_rate, ancestor_to_kid, mutation_count):
     if isinstance( ancestor_to_kid, list ):
@@ -95,28 +103,50 @@ def ancestor_to_kid(samples_id, depth):
         denominator = 2 ** (depth-1)
         ancestors = []
         ancestors_to_kids = {}
-        order = 0
-        for id in samples_id:
-            kid = np.where(samples_id == id)[0][0]
+        for i in range(len(samples_id)):
+            id = samples_id[i]
             prev_ancestor = int(id / denominator)
             ancestor = int(prev_ancestor / 2)
             if (ancestor + prev_ancestor) % 2 == 0:
                 continue
             else:
                 if ancestor in ancestors:
-                    ancestors_to_kids[order].append(kid)
+                    ancestors_to_kids[ancestors.index(ancestor)].append(i)
                 else:
-                    ancestors_to_kids[order] = [kid] 
-                    order += 1
                     ancestors.append(ancestor)
+                    ancestors_to_kids[ancestors.index(ancestor)] = [i]           
+        return ancestors_to_kids
 
 
 def get_mutation_per_molecule(samples_id, cycles, bases_per_amplicon, error_rate):
+    if samples_id is None:
+        return None 
+
+    n = len(samples_id)
     mutation_count = np.zeros([n, 2], dtype = np.int16)
 
     [ poisson_mutation(bases_per_amplicon, error_rate, ancestor_to_kid(samples_id, i), mutation_count) for i in range(cycles) ]
         
     return mutation_count
+
+def mylen(l):
+    if l is None:
+        return None
+    else:
+        return len(l)
+
+def simulation_per_well(well, n_molecules, n_mutated, cycles, dilution_rate, bases_per_amplicon, error_rate):
+    print("Processing Well:", well)
+    dilution_to = int( n_molecules * 2**UID_cycles * first_dilution_rate )
+    nsamples_per_molecule = nsamples_generator(np.ones(n_molecules), dilution_to)
+    samples_id = [sampleUID(cycles, num) for num in nsamples_per_molecule]
+    summary = [{"UIDs": translate(ids, cycles),
+                "n_samples": mylen(ids),
+                "mutated_before": i < n_mutated,
+                "molecule_id": i,
+                "well_id": well,
+                "mutations_count": get_mutation_per_molecule(ids, cycles, bases_per_amplicon, error_rate)} for i, ids in enumerate(samples_id)]
+    return summary
 
 if __name__ == '__main__':
     UID_cycles = 30
@@ -127,7 +157,9 @@ if __name__ == '__main__':
     mutated_count = 9
     wells = 94
     first_dilution_rate = 0.000185 * 0.01
+    sequencer_reads = 50000000
 
+    print("Begin0")
     molecule_per_well = [int(total_molecule / wells) for i in range(wells)]
     for i in range(wells):
         if sum(molecule_per_well) == total_molecule:
@@ -135,6 +167,7 @@ if __name__ == '__main__':
         else:
             molecule_per_well[i] += 1
 
+    print("Begin1")
     mutated_per_well = [0 for i in range(wells)]
     for i in range(mutated_count):
         loc = np.random.randint(wells)
@@ -143,42 +176,23 @@ if __name__ == '__main__':
         else:
             mutated_per_well[loc] += 1
 
-    all_well_samples_per_molecule = []
-    all_well_sampleUID = []
-    all_well_mutations = []
-    for i in range(wells):
-        print("Processing Well:", i)
-    
-        molecule_this_well = molecule_per_well[i]
-        mutated_this_well = mutated_per_well[i]
-
-        first_dilution_to = int( molecule_this_well * 2**UID_cycles * first_dilution_rate )
-
-        sampleUID_per_molecule = [0 for i in range(molecule_this_well)]
-        for i in range(first_dilution_to):
-            loc = np.random.randint(molecule_this_well)
-            sampleUID_per_molecule[loc] += 1
-        all_well_samples_per_molecule.append( sampleUID_per_molecule )
-        
-        samples = [ sampleUID(UID_cycles, s) for s in sampleUID_per_molecule ]
-        all_well_sampleUID.append( samples )
-
-        mutations = [ get_mutation(sample, UID_cycles, bases_per_amplicon, error_rate) for sample in samples ]
-        all_well_mutations.append( mutations )
+    print("Begin2")
+    summary_first_dilution = \
+    [simulation_per_well(w,n,m,UID_cycles,first_dilution_rate,bases_per_amplicon,error_rate)\
+                        for w, n, m in zip(range(wells), molecule_per_well, mutated_per_well)]
 
     count = 0
     for i in range(wells):
-        mutation_count_this_well = mutated_per_well[i]
-        molecule_this_well = molecule_per_well[i]
-        mutations_this_well = all_well_mutations[i]
-        nsamples_this_well = all_well_samples_per_molecule[i]
-        for j in range(molecule_this_well):
-            if j < mutation_count_this_well:
-                count += 2 * nsamples_this_well[j]
+        summary_this_well = summary_first_dilution[i]
+        for d in summary_this_well:
+            if d["UIDs"] != None and d["mutated_before"] == 1:
+                count += 2 * d["n_samples"]
             else:
-                count += np.sum( mutations_this_well[j] > 0 ) 
+                count += np.sum( d["mutations_count"] > 0 ) 
     
     print(count / (2 * total_molecule * 2**30 * first_dilution_rate))
+
+    # print(ancestor_to_kid(np.array([0,1,2,3,4,5,6,7]),0))
 
 
 
