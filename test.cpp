f2ad788a6a8bfdeae27c2dac9d6a15d79e0b95f6
IlyfTang <13300180071@fudan.edu.cn>
2018-04-13 18:40:37 -0400

    test2

--- test.cpp
+++ test.cpp
@@ -7,11 +7,14 @@ using namespace std;
 int main(){
     mt19937 eng{random_device{}()};
     vector<double> p(4);
+    vector<int> count(4);
     p[0] = 0.1;
     p[1] = 0.2;
     p[2] = 0.3;
     p[3] = 0.4;
-    int choice = discrete_distribution<int>(p.begin(),p.end())(eng);
-    cout<<choice<<endl;
+    for(int i=0;i<100;i++){
+        count [ discrete_distribution<int>(p.begin(),p.end())(eng) ] ++;
+    }
+    cout<<count[0]<<" "<<count[1]<<" "<<count[2]<<" "<<count[3]<<" "<<endl;
     return 0;
 }
